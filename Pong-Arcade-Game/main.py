c96261da1f9e76e8fd5345495dbc15bf238b026c
fiona <fiona.kitchen@yahoo.co.uk>
2023-02-02 14:21:22 +0000

    made my paddles and have them moving - down with class

--- Pong-Arcade-Game/main.py
+++ Pong-Arcade-Game/main.py
@@ -1,32 +1,30 @@
 from turtle import Screen, Turtle 
-
+from paddle import Paddle
 screen = Screen()
-
 screen.title("Welcome to Pong!")
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
+screen.tracer(0) # this combined with update stops the animation moving to place
 
-paddle = Turtle()
-paddle.color("white")
-paddle.shape('square')
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(x=350, y=0)
-
-
-def go_up():
-    new_y=paddle.ycor()+20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y=paddle.ycor()-20
-    paddle.goto(paddle.xcor(), new_y)
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350,0))
 
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(right_paddle.go_up, "Up")
+screen.onkey(right_paddle.go_down, "Down")
+screen.onkey(left_paddle.go_up, "w")
+screen.onkey(left_paddle.go_down, "s")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+
+
+
+
+
 
 
 screen.exitonclick()
