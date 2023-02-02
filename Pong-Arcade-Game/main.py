a26c89a0aea4700966b78507d30f8f3b850e7e3a
fiona <fiona.kitchen@yahoo.co.uk>
2023-02-02 14:32:42 +0000

    working on ball bouncing

--- Pong-Arcade-Game/main.py
+++ Pong-Arcade-Game/main.py
@@ -1,11 +1,15 @@
 from turtle import Screen, Turtle 
 from paddle import Paddle
+from ball import Ball
+import time
+
 screen = Screen()
 screen.title("Welcome to Pong!")
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.tracer(0) # this combined with update stops the animation moving to place
 
+ball = Ball()
 right_paddle = Paddle((350, 0))
 left_paddle = Paddle((-350,0))
 
@@ -19,7 +23,9 @@ screen.onkey(left_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 
