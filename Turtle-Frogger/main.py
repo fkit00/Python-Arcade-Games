bfff1cc517715b76c42759e7bee4007bf236773e
fiona <fiona.kitchen@yahoo.co.uk>
2023-02-06 13:46:59 +0000

    finished turtle crossing/fancy frogger

--- Turtle-Frogger/main.py
+++ Turtle-Frogger/main.py
@@ -9,12 +9,34 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 screen.listen()
 
+
+
 new_player=Player()
 screen.onkey(new_player.go_up,"Up")
 
+car_manager=CarManager()
+
+scoreboard= Scoreboard()
 
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_cars()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(new_player) < 20:
+            game_is_on= False
+            scoreboard.game_over()
+            
+    if new_player.is_at_finish_line():
+        new_player.go_to_start()       
+        # now we want to increase car speed!  
+        car_manager.level_up()
+        scoreboard.level_up()
+screen.exitonclick()
+
+
--- Turtle-Frogger/player.py
+++ Turtle-Frogger/player.py
@@ -16,4 +16,11 @@ class Player(Turtle):
         self.setheading(90)
     def go_up(self):
         self.forward(MOVE_DISTANCE)
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True 
+        else:
+            return False
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
 
